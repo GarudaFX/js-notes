fc9ca162bc02b59b89dae9c1dc6b64f4eb8a513e
nini is my bias <110276870+GarudaFX@users.noreply.github.com>
2022-07-30 17:48:01 +0800

    Update actual-practice.js

--- js-practice/notes-here/actual-practice.js
+++ js-practice/notes-here/actual-practice.js
@@ -90,17 +90,74 @@
 
 // console.log(sum(a,b));
 
-const groceries = ['apple','banana','orange','pair'];
-console.log(groceries);
-groceries.push('cookes');
-console.log(groceries.slice(1,4));
+// const groceries = ['apple','banana','orange','pair'];
+// console.log(groceries);
+// groceries.push('cookes');
+// console.log(groceries.slice(1,4));
 
-const person = {
-    name: 'Francis',
-    age: 20,
-    language: 'JavaScript'
+// const person = {
+//     name: 'Francis',
+//     age: 20,
+//     language: 'JavaScript',
+//     love: ' I love',
+//     hobbies: ' Programming',
+//     skills: function () {
+//         return this.love + this.hobbies;
+//     }
+// }
+
+// console.log(person.name); //dot notation
+// console.log(person['age']); //bracker notation
+// console.log(person.language);
+
+// person.phone = '090-874-79193'
+// console.log(person.phone);
+// console.log(person);
+
+// const person2 = {
+//     name: 'Jennie',
+//     age: 26,
+//     language: 'Korean'
+// }
+
+// console.log(person2.name); //dot notation
+// console.log(person2['age']); //bracker notation
+// console.log(person2.language);
+
+// const greeting = (person,person2) => {
+
+//     const hello = 'Hi my name is ' + person2.name + ' and my age is ' +
+//     person2.age + ' and I speak ' + person2.language + ' and mostly, I love ' + person.name + ' so much! and his motto is' + person.skills();
+
+//     return hello;
+// }
+
+// console.log(greeting(person,person2));
+
+//loops
+// const myHobbies = ['Programming','Coding','Development','Studying']
+// let i = 1;
+
+// for(i = 0; i < myHobbies.length; i++) {
+//     console.log(myHobbies[i]);
+// }
+
+// ES6 Function with ES6 for loop 
+// const numbers = []
+// let doubles = []
+// let sum = 0
+
+// const doubleNumbers = (numbers,) => {
+//     for(const number of numbers) {
+//         doubles.push(number ** 2)
+//     }
+//     return doubles;
+// }
+
+const letterCounter = (phrase) => {
+    return { result: phrase.length }
 }
 
-console.log(person.name);
-console.log(person['age']);
-console.log(person.language);
+const phrases = prompt('Write your phrase: ')
+console.log(phrases)
+console.log(letterCounter(phrases))
