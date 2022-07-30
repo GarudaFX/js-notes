e1b1fc40a4f728cd94266c23154a928a4212d0c1
GarudaFX <110276870+GarudaFX@users.noreply.github.com>
2022-07-30 14:24:58 +0800

    first commit

---
+++ js-practice/notes-here/actual-practice.js
@@ -0,0 +1,26 @@
+// var first_number = prompt('Enter first number: ');
+// var second_number = prompt('Enter second number: ');
+// var op = prompt('Enter operator: '); 
+
+// var result;
+// if(op == '+') {
+//     result = Number(first_number += second_number);
+//     alert(result);
+// } else if(op == '-') {
+//     result = first_number -= second_number;
+//     alert("Difference: " + result);
+// } else if(op == '*') {
+//     result = first_nubmer *= second_number;
+//     alert(result);
+// } else {
+//     result = first_number /= second_number;
+//     alert(result);
+// }
+
+// let question = prompt('Is it raining: ');
+
+// if(question == 'Yes'|| question == 'yes') {
+//     console.log('Grab your umbrella');
+// } else {
+//     console.log('Wear your sunglasses');
+// }
---
+++ js-practice/notes-here/notes.js
@@ -0,0 +1,20 @@
+// console.log(); - It is the print in terminal (where you can display results)
+// prompt(); - Inputs the user using User Interface
+// alert(); - Outputs the user using User Interface
+
+/* Opearotrs
+   //Basic operators
+   +, -, /, *, %, **
+
+    //Math Methods
+        //Math.ceil(value) - rounds up to the nearest integer
+        //Math.floor(variable) - rounds down to the nearest integer
+        //Math.random(value) - receive a number between 0 and 1
+*/
+
+/* Variables
+    const - The value cannot be changed
+    let = The value can be changed
+    var = The value can be changed (not recommended)
+*/ 
+
