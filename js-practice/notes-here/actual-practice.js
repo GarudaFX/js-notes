b137d62a90c49053158f32c9d02118762f91c06e
nini is my bias <110276870+GarudaFX@users.noreply.github.com>
2022-07-31 17:36:00 +0800

    Update actual-practice.js

--- js-practice/notes-here/actual-practice.js
+++ js-practice/notes-here/actual-practice.js
@@ -1,6 +1,6 @@
-// var first_number = prompt('Enter first number: ');
-// var second_number = prompt('Enter second number: ');
-// var op = prompt('Enter operator: '); 
+// // var first_number = prompt('Enter first number: ');
+// // var second_number = prompt('Enter second number: ');
+// // var op = prompt('Enter operator: '); 
 
 // var result;
 // if(op == '+') {
@@ -34,14 +34,14 @@
 // greetings('Francis');
 
 
-/*String Interpolation
-     function greeting(name) {
-         greet = `hi ${name}, Nice to meet you!`
-         console.log(greet);
-     }
+// /*String Interpolation
+//      function greeting(name) {
+//          greet = `hi ${name}, Nice to meet you!`
+//          console.log(greet);
+//      }
 
-     greeting('Jennie');
-*/
+//      greeting('Jennie');
+// */
 
 
 // let userPrompt1 = prompt("Enter num1: ");
@@ -60,33 +60,33 @@
 //     }
 // }
 
-// let result = calculate(userPrompt1, userPrompt2, userPrompt3);
-// console.log("Result: " + result);
+// // let result = calculate(userPrompt1, userPrompt2, userPrompt3);
+// // console.log("Result: " + result);
 
-// let a,b;
+// // let a,b;
 
-// const sum = (a,b) => {
-//     return a + b;
-// }
+// // const sum = (a,b) => {
+// //     return a + b;
+// // }
 
-// console.log(sum(1,3));
-// let a = 21, b = 10;
+// // console.log(sum(1,3));
+// // let a = 21, b = 10;
 
-// const sum = (a,b) => {
-//      return a+b;
-// }   
+// // const sum = (a,b) => {
+// //      return a+b;
+// // }   
 
-// const sub = (a,b) => {
-//     return a-b;
-// }
+// // const sub = (a,b) => {
+// //     return a-b;
+// // }
 
-// const div = (a,b) => {
-//     return a / b;
-// }
+// // const div = (a,b) => {
+// //     return a / b;
+// // }
 
-// const mul = (a,b) => {
-//     return a * b;
-// }
+// // const mul = (a,b) => {
+// //     return a * b;
+// // }
 
 // console.log(sum(a,b));
 
@@ -121,7 +121,7 @@
 // }
 
 // console.log(person2.name); //dot notation
-// console.log(person2['age']); //bracker notation
+// console.log(person2['age']); //tracker notation
 // console.log(person2.language);
 
 // const greeting = (person,person2) => {
@@ -134,7 +134,7 @@
 
 // console.log(greeting(person,person2));
 
-//loops
+// //loops
 // const myHobbies = ['Programming','Coding','Development','Studying']
 // let i = 1;
 
@@ -142,7 +142,7 @@
 //     console.log(myHobbies[i]);
 // }
 
-// ES6 Function with ES6 for loop 
+// // ES6 Function with ES6 for loop 
 // const numbers = []
 // let doubles = []
 // let sum = 0
@@ -154,10 +154,164 @@
 //     return doubles;
 // }
 
-const letterCounter = (phrase) => {
-    return { result: phrase.length }
-}
+// const letterCounter = (phrase) => {
+//     return { result: phrase.length }
+// }
 
-const phrases = prompt('Write your phrase: ')
-console.log(phrases)
-console.log(letterCounter(phrases))
+// const phrases = prompt('Write your phrase: ')
+// console.log(phrases)
+// console.log(letterCounter(phrases))
+
+// const myNum = [1,2,3,4]
+
+// const sumOfMyNum = (myNum) => {
+//     let sum = 0
+
+//     for(const num of myNum) {
+//         sum+=num
+//     }
+//     return {result: sum}
+// }
+
+
+//------------------------------------------------------------------------------
+
+
+// console.log(sumOfMyNum(myNum))
+
+//Finding the largest number inside
+//the array
+// const nums = [] 
+
+// const max = (nums) => {
+//     let result = nums[0]
+    
+//     for(const number of nums) {
+//         if(number > result) result = number
+//     }
+//     return result
+// }
+
+// console.log(max([4,2,6,9,1,54,234,621,423,52432]));
+
+//--------------------------------------------------------------------------------
+
+
+//Determine the frequency of the string 
+// const phrase = []
+
+// const wordFrequency = (phrase) => {
+//     //constructing an object to store the letter and its frequency 
+//     let frequency = {}
+//     for(const letter of phrase) {
+//         //check if the letter is already existing
+//         if(letter in frequency) {
+//             frequency[letter] += 1
+//         } else {
+//             frequency[letter] = 1
+//         }
+//     }
+//     return frequency
+// }
+// console.log(letterFrequency('balay ni labay libat'))
+
+
+//--------------------------------------------------------------------------------
+
+//Function to determine the frequency of words inside a phrase
+
+// const wordFrequency = (array) => {
+//     let frequency = {}
+//     const wordArray = phrase.split(' ') //split function ables you to divide strings into different indeces inside an array
+//     // for(const word of wordArray) {
+//     //     console.log(word)
+//     // }
+//     console.log(wordArray)
+// }
+
+// wordFrequency('lmao what the heck lmao')
+
+
+//---------------------------------------
+
+// const numbers = []
+// const greaterThan = 3
+
+
+// const filter = (numbers,greaterThan) => {
+//     numbers.map(number => {
+//         if(number > greaterThan)
+//             empty.push(number)
+//     })
+//     console.log(empty)
+
+//     for(number of numbers) {
+//         if(number > greaterThan) 
+//             empty.push(number)
+//     }
+//     return empty
+// } 
+
+// //-------------------------------------
+// //Array of objects
+
+// const actors = [
+//     {
+//         name: 'Francis',
+//         age: 20,
+//         networth: 160
+//     },
+//     {
+//         name: 'Sophia',
+//         age: 19,
+//         networth: 170
+//     },
+//     {
+//         name: 'Jennie',
+//         age: 19,
+//         networth: 170
+//     },
+//     {
+//         name: 'Rose',
+//         age: 19,
+//         networth: 170
+//     },
+//     {
+//         name: 'Lisa',
+//         age: 19,
+//         networth: 170
+//     },
+//     {
+//         name: 'Jisoo',
+//         age: 19,
+//         networth: 170
+//     }
+// ]
+// const template = document.getElementById("template")
+
+// let result = actors.filter(actor => actor.networth > 150)
+// let names = result.map(actor => actor.name).join(', ')
+// template.innerHTML = `<h1>${(names)}</h1>`
+
+
+//---------------------------------------------------- CLicker
+// const displayCount = document.getElementById("counter")
+// const buttons = document.querySelectorAll(".clicker")
+
+// const choice = {
+//     add : 0,
+//     deduct: function() {
+//         return this.add = this.add - 1
+//     }
+// }
+
+// buttons.forEach(button => {
+//     button.onclick = () => {
+//         if(button.value == "add") {
+//             choice[button.value] += 1
+//         } else {
+//             choice.deduct()
+//         }
+//         displayCount.innerText = `${choice[button.value]}`
+//     }
+// })
